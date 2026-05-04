b6af5afeeee39a2a03e8eaeabc25abc5ff4f4bfe
ghostm0nk <139760390+ghostm0nk@users.noreply.github.com>
2026-05-04 19:28:50 +0600

    Add src/App.jsx

---
+++ src/App.jsx
@@ -0,0 +1,32 @@
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import Header from './components/Header';
+import Footer from './components/Footer';
+import Home from './pages/Home';
+import Submit from './pages/Submit';
+import Businesses from './pages/Businesses';
+import BusinessProfile from './pages/BusinessProfile';
+import About from './pages/About';
+import Contact from './pages/Contact';
+
+function App() {
+  return (
+    <Router>
+      <div className="min-h-screen flex flex-col bg-gray-50">
+        <Header />
+        <main className="flex-grow">
+          <Routes>
+            <Route path="/" element={<Home />} />
+            <Route path="/submit" element={<Submit />} />
+            <Route path="/businesses" element={<Businesses />} />
+            <Route path="/business/:id" element={<BusinessProfile />} />
+            <Route path="/about" element={<About />} />
+            <Route path="/contact" element={<Contact />} />
+          </Routes>
+        </main>
+        <Footer />
+      </div>
+    </Router>
+  );
+}
+
+export default App;
